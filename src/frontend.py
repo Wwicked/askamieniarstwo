3c4a19741a112327d7e32606d900cbd48e172d81
Wicked <wwicked@o2.pl>
2019-12-11 14:23:05 +0100

    Added menu bar

--- src/frontend.py
+++ src/frontend.py
@@ -10,4 +10,23 @@ class Gui:
 		# Set window title
 		self.window.wm_title("AS Kamieniarstwo")
 
+		self.mainWindow()
 		self.window.mainloop()
+
+	def mainWindow(self):
+		menuBar = Menu(self.window)
+
+		fileMenu = Menu(menuBar, tearoff = 0)
+		fileMenu.add_command(label = "Nowy plik", command = self.newFile)
+		fileMenu.add_separator()
+		fileMenu.add_command(label = "Wyjdz", command = self.exit)
+
+		menuBar.add_cascade(label = "Plik", menu = fileMenu)
+
+		self.window.config(menu = menuBar)
+
+	def newFile(self):
+		pass
+
+	def exit(self):
+		pass
